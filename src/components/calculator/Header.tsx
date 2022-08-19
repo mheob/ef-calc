165f282725a106cf1fb61627c2ed4a0a20e2f83d
Alex Böhm <tools@boehm.work>
2022-08-19 14:23:42 +0200

    feat: add calculation output (#7)
    Render the current calculation in the header of the calculator.

--- src/components/calculator/Header.tsx
+++ src/components/calculator/Header.tsx
@@ -1,9 +1,13 @@
 import efLogo from '../../assets/easyfeedback-logo-inverted.svg';
+import { useStore } from '../../stores/root.store';
 
 export function Header(): JSX.Element {
+  const calculation = useStore((state) => state.calculation);
+
   return (
     <header className="Calculator__header">
       <img className="Calculator__header-image" src={efLogo} alt="easyfeedback logo" />
+      <output className="Calculator__header-last">{calculation}</output>
     </header>
   );
 }
